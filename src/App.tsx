66544c8719e0271e6f7f5adcb28720c9159a76a0
Guilherme Farias <guicfarias11@gmail.com>
2021-03-01 13:10:37 -0300

    Adding a Global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-function App() {
-  return (
-    <h1>Hello NLW</h1>
-  );
-}
+import React from 'react';
+import './styles/global.css';
+
+const App: React.FC = () => {
+  return <h1>Hello NLW</h1>;
+};
 
 export default App;
