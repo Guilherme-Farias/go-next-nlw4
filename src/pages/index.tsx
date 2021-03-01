8ac52643b27243b51417f7e972cdd41424986235
Guilherme Farias <guicfarias11@gmail.com>
2021-03-01 14:44:11 -0300

    Finished Profile Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import styles from '../styles/pages/Home.module.css'
 import ExperienceBar from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <h1>Hello Next.JS</h1>
           <ExperienceBar />
-          <h1>Hello Next.JS</h1>
+          <section>
+            <div>
+              <Profile/>
+            </div>
+            <div>
+
+            </div>
+          </section>
     </div>
   )
 }
