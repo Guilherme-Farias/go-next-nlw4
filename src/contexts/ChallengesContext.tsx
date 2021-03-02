06a8e3280d5b78ba09b1085b6020ca159dd84fd3
Guilherme Farias <guicfarias11@gmail.com>
2021-03-02 13:36:37 -0300

    Finished ChallengeContext

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState, ReactNode } from 'react';
+
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    console.log('new challenger');
+  }
+
+  return (
+    <ChallengesContext.Provider value={{
+      level,
+      currentExperience,
+      challengesCompleted,
+      startNewChallenge,
+      levelUp,
+    }}>
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
