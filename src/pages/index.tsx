f5bc13965aebd0f6cc38050f037a0a4c4c902b23
Guilherme Farias <guicfarias11@gmail.com>
2021-03-02 11:32:49 -0300

    Finished Countdown Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css'
 import ExperienceBar from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next.JS</h1>
-          <ExperienceBar />
-          <section>
-            <div>
-              <Profile/>
-              <CompletedChallenges/>
-            </div>
-            <div>
+    <>
+      <Head>
+        <title>Move it</title>
+      </Head>
+      <div className={styles.container}>
+        <ExperienceBar />
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
 
-            </div>
-          </section>
-    </div>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
